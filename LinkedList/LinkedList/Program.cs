e905bc0d5fadfc9646a3999862e2207884d55460
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-24 00:15:21 +0530

    [Amol] : Adding For Simple LinkedList

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Linked linked = new Linked();
+            bool flag = true;
+            Console.WriteLine("choose one : ");
+            int option = Convert.ToInt32(Console.ReadLine());
+           
+            while (flag)
+                switch (option)
+                {
+                    case 1:
+                         linked.Add(56);
+                         linked.Add(30);
+                         linked.Add(70);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+        }
+        }
+    }
+}
